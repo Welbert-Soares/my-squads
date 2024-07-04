a10d7c159654d1204543d22756a7bbc7bf23577f
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-07-04 20:25:37 -0300

    NewGround Screens

---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,24 @@
+import { Header } from "@components/Header";
+import { Container, Content, Icon } from "./styles";
+import { Highlight } from "@components/Highlight";
+import { Button } from "@components/Button";
+
+export const NewGroup = () => {
+  return (
+    <Container>
+      <Header showBackButton />
+
+      <Content>
+        <Icon />
+        <Highlight 
+          title="Nova turma"
+          subtitle="Crie a turma para adicionar as pessoas"
+        />
+
+        <Button 
+          title="Criar"
+        />
+      </Content>
+    </Container>
+  )
+}
