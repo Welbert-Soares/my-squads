d676a3986688a79128cd8d185d64a45509b56b3d
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-07-04 20:01:23 -0300

    Button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -8,6 +8,7 @@ import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 
 export function Groups() {
@@ -36,6 +37,10 @@ export function Groups() {
             message="Que tal cadastrar a primeira turma?"
           />
         )}
+      />
+    
+      <Button 
+        title="Criar nova turma"
       />
     </Container>
   );
