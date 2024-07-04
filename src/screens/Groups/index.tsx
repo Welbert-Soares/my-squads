b8997183d4b46c9113af4e62ed33676bd6155a1a
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-07-03 22:13:37 -0300

    config types

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,13 +1,10 @@
 
 
-import { Container, Title } from './styles';
+import { Container} from './styles';
 
 export function Groups() {
   return (
     <Container>
-      <Title>
-        Groups
-      </Title>
     </Container>
   );
 }
