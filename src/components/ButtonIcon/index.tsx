76247c9e1f74764276486d898dfab1d34ff68bde
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-07-04 22:15:41 -0300

    deixando icone dinamico

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,17 +1,19 @@
 import { TouchableOpacityProps } from "react-native";
+import { MaterialIcons} from "@expo/vector-icons";
 
-import { Container, Icon } from "./styles";
+import { Container, Icon, ButtonIconTypeStyleProps } from "./styles";
 
 type Props = TouchableOpacityProps & {
-
+  icon: keyof typeof MaterialIcons.glyphMap;
+  type?: ButtonIconTypeStyleProps;
 }
 
-export const ButtonIcon = ({}: Props) => {
+export const ButtonIcon = ({ icon, type = "PRIMARY", ...rest }: Props) => {
   return (
     <Container>
       <Icon 
-        name="home" 
-        type="PRIMARY"
+        name={icon} 
+        type={type}
       />
     </Container>
   );
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,8 +1,9 @@
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
 
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { Input } from "@components/Input";
 
 export const Players = () => {
   return (
@@ -14,7 +15,15 @@ export const Players = () => {
         subtitle="adicione a galera e separe os times"
       />
 
-      <ButtonIcon />
+      <Form>
+        <Input 
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+        />
+        <ButtonIcon 
+          icon="add" 
+        />
+      </Form>
     </Container>
   );
 };
