35bdf2df8bd90419441186ca735edc1b0a856699
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-07-04 21:46:15 -0300

    criando interface de players

---
+++ src/screens/Players/index.tsx
@@ -0,0 +1,18 @@
+import { Container } from "./styles";
+
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+
+export const Players = () => {
+  return (
+    <Container>
+      <Header showBackButton />
+
+      <Highlight 
+        title="Nome da turma"
+        subtitle="adicione a galera e separe os times"
+      />
+      
+    </Container>
+  );
+};
