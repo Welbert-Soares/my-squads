440085b98ad0f7f753b417a934105a8ad4d0b280
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-07-05 19:37:16 -0300

    Armazenando Players

---
+++ src/storage/play/PlayerStorageDTO.ts
@@ -0,0 +1,6 @@
+
+
+export type PlayerStorageDTO = {
+  nome: string;
+  team: string;
+}
--- src/storage/storageConfig.ts
+++ src/storage/storageConfig.ts
@@ -1,3 +1,4 @@
 const GROUP_COLLECTION = "my-squads: groups";
+const PLAYER_COLLECTION = "my-squads: players";
 
-export { GROUP_COLLECTION };
+export { GROUP_COLLECTION, PLAYER_COLLECTION };
